7b0d0b051ad131844035138664dbc601c87519d4
Alexander <7319abowers@gmail.com>
2022-05-21 23:58:43 -0700

    forward declared Capsule Component on Base Pawn.

--- BasePawn.cpp
+++ BasePawn.cpp
@@ -2,20 +2,20 @@
 
 
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
 }
 
 // Called when the game starts or when spawned
 void ABasePawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
