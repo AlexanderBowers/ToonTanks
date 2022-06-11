7412bce3d6cd10db8af978debebdb2e2031ce860
Alexander <7319abowers@gmail.com>
2022-06-11 15:57:55 -0700

    Added Tower class

---
+++ Tower.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tower.h"
+
+void ATower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Find the distance to the Tank
+
+	// Check to see if the Tank is in range
+
+	//If in range, rotate turret towards Tank
+
+}
+
