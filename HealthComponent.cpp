7aa547e15c1f4b3a2f3ab1f3cb1e91631d2165bb
Alexander <7319abowers@gmail.com>
2022-06-12 12:34:32 -0700

    Projectiles damage health then destroy

--- HealthComponent.cpp
+++ HealthComponent.cpp
@@ -2,6 +2,7 @@
 
 
 #include "HealthComponent.h"
+#include "GameFramework/Controller.h"
 
 // Sets default values for this component's properties
 UHealthComponent::UHealthComponent()
@@ -19,8 +20,8 @@ void UHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	Health = MaxHealth;
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::DamageTaken);
 }
 
 
@@ -32,3 +33,10 @@ void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActo
 	// ...
 }
 
+void UHealthComponent::DamageTaken(AActor* DamagedActor, float Damage, const UDamageType* DamageType, class AController* Instigator, AActor* DamageCauser)
+{
+	if (Damage <= 0.f) return;
+	
+	Health -= Damage;
+	UE_LOG(LogTemp, Warning, TEXT("Health: %f"), Health);
+}
